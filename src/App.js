8e4de0c8c42c10c0f8a9757b2dfdbdfef868e79f
abc <abc@gmail.com>
2023-06-17 22:50:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [name, setName] = useState("");
+  let [pass, setPass] = useState("");
+  let [mail, setMail] = useState("");
+  let [conpass, setconPass] = useState("");
+  let [error, seterror] = useState("");
+  let [success, setsucc] = useState("");
+  function Submit(){
+      if(name === "" || pass === "" || mail === "" || conpass === ""){
+        setsucc("")
+        seterror("Error: All the fileds are mandatory")
+      }
+      else if(pass !== conpass){
+        setsucc("")
+        seterror("Error: Confirm Password must be same as Password")
+      }
+      else{
+        seterror("")
+        setsucc("Success: Successfully Signed up")
+      }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='head'>Signup</h1>
+      <input className='name' type='text'
+        onChange={(e) => setName(e.target.value)} value={name} placeholder='Full Name'>
+      </input>
+      <input className='email' type='text'
+       onChange={(e) => setMail(e.target.value)} value={mail} placeholder='Email'>
+      </input>
+      <input className='Password' type='text'
+       onChange={(e) => setPass(e.target.value)} value={pass} placeholder='Password'>
+      </input>
+      <input className='Con-Password' type='text' 
+       onChange={(e) => setconPass(e.target.value)} value={conpass} placeholder='Confirm Password'>
+      </input>
+      <div className='err'>{error}</div>
+
+      <div className='succ'>{success}</div>
+      <button onClick={Submit}>Submit</button>
     </div>
   );
 }
